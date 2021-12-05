03f427da196808cfd4062ab50e9a73fbe488dc1f
jashughes <jasmine.hughes@berkeley.edu>
2021-12-04 22:57:31 -0800

    rm deepcopy calls

--- 03.jl
+++ 03.jl
@@ -9,8 +9,8 @@ scan_array(a, i, f) = a[:, a[i, :] .== Int(f(sum(a[i, :]), size(a, 2)/2))]
 function solve(input)
     o2, co2 = input, input
     for i = 1:size(input, 1)
-        if (size(o2, 2) > 1)  o2 = scan_array(deepcopy(o2), i, >=) end
-        if (size(co2, 2) > 1) co2 = scan_array(deepcopy(co2), i, <) end
+        if (size(o2, 2) > 1)  o2 = scan_array(o2, i, >=) end
+        if (size(co2, 2) > 1) co2 = scan_array(co2, i, <) end
     end
     array_to_binary(o2) * array_to_binary(co2)
 end
