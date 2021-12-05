f92bad61d9f4ffaaa0199514ed588670eb9d1174
jashughes <jasmine.hughes@berkeley.edu>
2021-12-04 23:51:32 -0800

    more idiomatic vectorization

--- 03.jl
+++ 03.jl
@@ -2,7 +2,7 @@ input = hcat([parse.(Int, split(i, "")) for i in readlines("03.txt")]...)
 
 # part 1
 array_to_binary(a) = parse(Int, join(a, ""), base = 2)
-γε(i, f) = Int.(f(sum(i, dims = 2), size(i, 2)/2))
+γε(i, f) = Int.(f.(sum(i, dims = 2), size(i, 2)/2))
 
 # part 2
 scan_array(a, i, f) = a[:, a[i, :] .== Int(f(sum(a[i, :]), size(a, 2)/2))]
@@ -15,5 +15,5 @@ function solve(input)
     array_to_binary(o2) * array_to_binary(co2)
 end
 
-println("Part 1: ", array_to_binary(γε(input, .>)) * array_to_binary(γε(input, .<)))
+println("Part 1: ", array_to_binary(γε(input, >)) * array_to_binary(γε(input, <)))
 println("Part 2: ", solve(input))
