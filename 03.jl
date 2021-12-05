183ea1ed876548dfb8247f60ff6d6df87559bd47
jashughes <jasmine.hughes@berkeley.edu>
2021-12-04 20:53:35 -0800

    very tidy part 2 solution :)

--- 03.jl
+++ 03.jl
@@ -1,35 +1,20 @@
 input = hcat([parse.(Int, split(i, "")) for i in readlines("03.txt")]...)
 
-function count_pos(a)
-    [sum(x) for x in eachrow(hcat(a...))]
-end
-
-function scan_array(a, i, f)
-    counts = count_pos(a)
-    ci = Int(f(counts[i], length(a)/2))
-    filter(x -> x[i] == ci, a)
-end
-
 # part 1
 array_to_binary(a) = parse(Int, join(a, ""), base = 2)
 gamma  = array_to_binary(Int.(sum(input, dims = 2) .> size(input, 2)/2))
 epsilon = array_to_binary(Int.(sum(input, dims = 2) .< size(input, 2)/2))
 
 # part 2
+scan_array(a, i, f) = a[:, a[i, :] .== Int(f(sum(a[i, :]), size(a, 2)/2))]
 function solve(input)
     o2, co2 = input, input
-    for i = 1:length(input[1])
-        if length(o2) > 1
-            o2 = scan_array(o2, i, >=)
-        end
-        if length(co2) > 1
-            co2 = scan_array(co2, i, <)
-        end
+    for i = 1:size(input, 1)
+        if (size(o2, 2) > 1)  o2 = scan_array(deepcopy(o2), i, >=) end
+        if (size(co2, 2) > 1) co2 = scan_array(deepcopy(co2), i, <) end
     end
-    o2[1], co2[1]
+    array_to_binary(o2) * array_to_binary(co2)
 end
 
-o2, co2 = solve(input)
-
 println("Part 1: ", gamma * epsilon)
-println("Part 2: ", array_to_binary(o2) * array_to_binary(co2))
+println("Part 2: ", solve(input))
