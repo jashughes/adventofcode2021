976b70b5f22b357844195bd5c2b24e1828db4857
jashughes <jasmine.hughes@berkeley.edu>
2021-12-05 22:34:40 -0800

    sum over subsetting

--- 06.jl
+++ 06.jl
@@ -2,7 +2,7 @@
 input = parse.(Int, split(readlines("06.txt")[1], ","))
 
 function counting_fish(input, days)
-    ages = [length(input[input .== x]) for x in 0:8]
+    ages = [sum(input .== x) for x in 0:8]
     for d = 1:days
         baby_fish = ages[1]
         ages = ages[2:end]
