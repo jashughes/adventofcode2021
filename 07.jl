bf741795260d40b00de40826f020a7ce59e1a4cb
jashughes <jasmine.hughes@berkeley.edu>
2021-12-06 21:55:37 -0800

    more characters, fewr for-loops

--- 07.jl
+++ 07.jl
@@ -6,5 +6,5 @@ println("Part 1: ", Int(sum(abs.(input .- median(input)))))
 
 println(
     "Part 2: ", 
-    minimum((sum(sum(1:abs(i - m)) for i in input)) for m  = 1:length(input))
+    minimum(sum(sum.(range.(1, abs.(input .- m), step = 1)) for m = 1:length(input)))
 )
