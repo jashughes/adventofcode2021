45f0cd2370fb61af9d3cdc05446366da32123d30
jashughes <jasmine.hughes@berkeley.edu>
2021-12-06 21:32:19 -0800

    one liners

--- 07.jl
+++ 07.jl
@@ -4,13 +4,7 @@ input = parse.(Int, split(readlines("07.txt")[1], ","))
 
 println("Part 1: ", Int(sum(abs.(input .- median(input)))))
 
-function solve(input)
-    min_move = Inf
-    for m = 1:length(input)
-        moves = sum(sum(1:abs(i - m)) for i in input)
-        min_move  = Int(minimum([min_move, moves]))
-    end
-    min_move
-end
-
-println("Part 2: ", solve(input))
+println(
+    "Part 2: ", 
+    minimum((sum(sum(1:abs(i - m)) for i in input)) for m  = 1:length(input))
+)
