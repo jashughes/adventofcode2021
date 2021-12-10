2bdb619302050d55b784261795231fa8539d8acf
jashughes <jasmine.hughes@berkeley.edu>
2021-12-09 22:37:42 -0800

    reduce for costing pt2

--- 10.jl
+++ 10.jl
@@ -2,18 +2,10 @@ input = split.(readlines("10.txt"), "")
 
 op = Dict("(" => ")", "[" => "]", "{" => "}", "<" => ">")
 prices1 = Dict(")" => 3, "]" => 57, "}" => 1197, ">" => 25137)
-prices2 = Dict(")" => 1, "]" => 2, "}" => 3, ">" => 4)
+prices2 = Dict("(" => 1, "[" => 2, "{" => 3, "<" => 4)
 halfway(arr) = arr[Int(ceil(length(arr)/2))]
 
-function cost(arr, tab = prices2)
-    score = 0
-    for a in arr
-        score = (score * 5) + tab[a]
-    end
-    score
-end
-
-function cost_of_corruption(str, op = op)
+function cost_of_corruption(str, op = op, p1 = prices1, p2 = prices2)
     brackets = []
     for s in str
         if s in keys(op)
@@ -21,10 +13,10 @@ function cost_of_corruption(str, op = op)
         elseif s == op[brackets[end]]
             pop!(brackets)
         else
-            return [true, prices1[s]]
+            return [true, p1[s]]
         end
     end
-    [false, cost([op[x] for x in reverse(brackets)])]
+    [false, reduce((a,b) -> a * 5 + p2[b], reverse(brackets), init = 0)]
 end
 
 costs = [cost_of_corruption(s) for s in input]
