9d2cfa6d9b9b8d0176176bee05ebef64e6460c1e
jashughes <jasmine.hughes@berkeley.edu>
2021-12-11 08:12:30 -0800

    suggestions form twitter code review

--- 10.jl
+++ 10.jl
@@ -1,11 +1,9 @@
 input = split.(readlines("10.txt"), "")
 
 op = Dict("(" => ")", "[" => "]", "{" => "}", "<" => ">")
-prices1 = Dict(")" => 3, "]" => 57, "}" => 1197, ">" => 25137)
-prices2 = Dict("(" => 1, "[" => 2, "{" => 3, "<" => 4)
-halfway(arr) = arr[Int(ceil(length(arr)/2))]
+prices = Dict(")"=> 3,"]"=> 57,"}"=>1197,">"=>25137,"("=> 1,"["=>2,"{"=> 3,"<" =>4)
 
-function cost_of_corruption(str, op = op, p1 = prices1, p2 = prices2)
+function cost_of_corruption(str, op = op, p = prices)
     brackets = []
     for s in str
         if s in keys(op)
@@ -13,13 +11,13 @@ function cost_of_corruption(str, op = op, p1 = prices1, p2 = prices2)
         elseif s == op[brackets[end]]
             pop!(brackets)
         else
-            return [true, p1[s]]
+            return [true, p[s]]
         end
     end
-    [false, reduce((a,b) -> a * 5 + p2[b], reverse(brackets), init = 0)]
+    [false, reduce((a,b) -> a * 5 + p[b], reverse(brackets), init = 0)]
 end
 
 costs = [cost_of_corruption(s) for s in input]
 
 println("Part 1: ", sum(filter(x -> x[1] == 1, costs))[2])
-println("Part 2: ", halfway(sort(filter(x -> x[1] == 0, costs)))[2])
+println("Part 2: ", sort(filter(x -> x[1] == 0, costs))[end÷2 + 1][2])
