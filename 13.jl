f17210567f1cf54b18eb78a543d99f4460e93eb2
jashughes <jasmine.hughes@berkeley.edu>
2021-12-12 22:09:01 -0800

    initial solution

---
+++ 13.jl
@@ -0,0 +1,37 @@
+input = readlines("13.txt")
+div = 861
+#div = 18
+dots = [parse.(Int, x) .+ 1 for x in split.(input[1:div], ",")]
+dotCI = [CartesianIndex((d...)) for d in dots]
+folds = input[div + 2:end]
+sheet = Matrix{Int64}(zeros(maximum(hcat(dots...), dims = 2)...))
+sheet[dotCI] .= 1
+
+foldline(instr) = parse(Int, match(r"([0-9]+)", instr)[1]) + 1
+folddir(instr) = contains(instr, r"x=") ? 1 : 2
+
+function fold(sheet, instr)
+    l, d = foldline(instr), folddir(instr)
+    if d == 1
+        sheet[1:l-1,:] + reverse(sheet[l+1:end,:], dims = d)
+    else
+        sheet[:,1:l-1] + reverse(sheet[:,l+1:end], dims = d)
+    end
+end
+
+function origami(sheet, folds)
+    for f in folds
+        sheet = fold(sheet, f)
+    end
+    sheet
+end
+
+function printmanual(manual)
+    for j = 1:size(manual)[2]
+        println(join(ifelse.(manual[:,j] .> 0, "#", " "), ""))
+    end
+end
+
+println("Part 1: ", sum(fold(sheet, folds[1]) .> 0))
+println("Part 2:")
+printmanual(origami(sheet, folds))
