6f36031fec9ff085d2d3c0fe2336635a6892d31a
jashughes <jasmine.hughes@berkeley.edu>
2021-12-15 10:02:35 -0800

    rm unused libraries, variables

--- 15.jl
+++ 15.jl
@@ -1,6 +1,8 @@
-using MetaGraphs, SimpleWeightedGraphs, Graphs
+using SimpleWeightedGraphs, Graphs
 input = hcat([parse.(Int, s) for s in split.(readlines("15.txt"), "")]...)
 CI = CartesianIndex
+
+path_weights(p, mg) = [get_weight(mg, p[i-1], p[i]) for i in 2:length(p)]
 
 function idx(p, n)
     dirs = [(0,1), (0,-1), (1,0), (-1,0)]
@@ -17,14 +19,8 @@ function createGraph(input)
             add_edge!(mg, lu[ci], lu[n], input[n])
         end
     end
-    mg, lu, lu[CI((1,1))], lu[CI((size(input)...))]
+    mg, lu[CI((1,1))], lu[CI((size(input)...))]
 end
-
-path_weights(p, mg) = [get_weight(mg, p[i-1], p[i]) for i in 2:length(p)]
-
-mg, lu, _start, _end = createGraph(input)
-path = filter(x -> length(x) > 0 && x[end] == _end, enumerate_paths(dijkstra_shortest_paths(mg, _start)))[1]
-println("Part 1: ", Int(sum(path_weights(path, mg))))
 
 function cave_row(start_chamber, n)
     cave, new_chamber = deepcopy(start_chamber), deepcopy(start_chamber)
@@ -48,10 +44,11 @@ function bigger_cave(input, n = 5)
     big_cave
 end
 
-big_cave = bigger_cave(input)
-mg, lu, _start, _end = createGraph(big_cave)
-path = filter(x -> length(x) > 0 && x[end] == _end, enumerate_paths(dijkstra_shortest_paths(mg, _start)))[1]
-println("Part 2: ", Int(sum(path_weights(path, mg))))
+function solve(input)
+    mg, _s, _e = createGraph(input)
+    path = filter(x -> length(x) > 0 && x[end] == _e, enumerate_paths(dijkstra_shortest_paths(mg, _s)))[1]
+    Int(sum(path_weights(path, mg)))
+end
 
-# not 2185
-
+println("Part 1: ", solve(input))
+println("Part 2: ", solve(bigger_cave(input)))
