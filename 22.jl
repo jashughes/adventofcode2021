bc95a9be0392f16ca6faac82e96f7a62b97592e0
jashughes <jasmine.hughes@berkeley.edu>
2022-01-01 14:25:37 -0800

    mountain - ty @rodericday

--- 22.jl
+++ 22.jl
@@ -1,57 +1,42 @@
 st2ints(s) = [parse(Int, s[f]) for f in findall(r"(-*[0-9]+)", s)] 
-UniqSpts(arr, r) = sort(unique(vcat([x[2][r] for x in arr]...)))
-tuplein(t1, t2) = (t1[1] >= t2[1] && t1[2] <= t2[2])
-vol(t1,t2,t3) = (t1[2] - t1[1] + 1) * (t2[2] - t2[1] + 1) * (t3[2] - t3[1] + 1) 
-tupled(arr) = [(arr[1], arr[2]), (arr[3], arr[4]), (arr[5], arr[6])]
+input = [(match(r"([a-z]+) ", s)[1] == "on", st2ints(s)) for s in readlines("22.txt")]
 
-input = [(match(r"([a-z]+) ", s)[1], st2ints(s)) for s in readlines("22.txt")]
-instr, coords = first.(input), last.(input)
+function netVolume(cubes)
+    if length(cubes) == 0 return 0 end
+    if length(cubes) == 1 return cubes[1][1] * volume(cubes[1][2]...) end
 
-# Part 1
-function flicker(instr, coords, limit = 50)
-    initproc = findall(x -> maximum(abs.(x)) <= limit, coords)
-    cs = [c .+ limit for c in coords[initproc]]
-    is = instr[initproc]
-    m = zeros(Int64, 2 * limit, 2 * limit, 2 * limit)
+    (is_on, cube), rest = cubes[1], cubes[2:end]
+    if !is_on return netVolume(rest) end
+    itxs = itx(cube, rest)
 
-    for (i, c) in zip(is, cs)
-        m[c[1]:c[2], c[3]:c[4], c[5]:c[6]] .= i == "on" ? 1 : 0
-    end
-    sum(m)
+    volume(cube...) - netVolume(itxs) + netVolume(rest)
 end
 
-# Part 2
-function chunk(input)
-    xs, ys, zs = UniqSpts(input, 1:2), UniqSpts(input, 3:4), UniqSpts(input, 5:6)
-    append!(xs, xs .+ 1), append!(ys, ys .+ 1), append!(zs, zs .+ 1)
-    xs, ys, zs = sort(xs), sort(ys), sort(zs)
-    tot = 0
-    
-    for xi = 2:length(xs)
-        println(xi)
-        x1,x2 = xs[xi-1:xi] .+ [0, -1]
-        for yi = 2:length(ys)
-            y1,y2 = ys[yi-1:yi] .+ [0, -1]
-            for zi = 2:length(zs)
-                z1,z2 = zs[zi-1:zi] .+ [0, -1]
-                pti = [(x1,x2),(y1,y2),(z1,z2)]
-                reg = 0
-                for i in input
-                    instr, p = i[1], tupled(i[2])
-                    if all([tuplein(t1,t2) for (t1, t2) in zip(pti, p)])
-                        if instr == "on"
-                            reg = 1
-                        else
-                            reg = 0
-                        end
-                    end
-                end
-                tot += reg * vol(pti...)
-            end
+function itx(cube, rest)
+    itxs = []
+    for (_, other) in rest
+        newCube = tinyCube(cube, other)
+        if volume(newCube...) > 0
+            append!(itxs, [(true, newCube)])
         end
     end
-    tot
+    itxs
 end
 
-println("Part 1: ", flicker(instr, coords))
-println("Part 2: ", chunk(input))
+function tinyCube(cube, other)
+    [
+        max(cube[1], other[1]),
+        min(cube[2], other[2]),
+        max(cube[3], other[3]),
+        min(cube[4], other[4]),
+        max(cube[5], other[5]),
+        min(cube[6], other[6])
+    ]
+end
+
+function volume(X1,X2,Y1,Y2,Z1,Z2)
+    max(X2 - X1 + 1, 0) * max(Y2 - Y1 + 1, 0) * max(Z2 - Z1 + 1, 0)
+end
+
+println("Part 1: ", netVolume(input[1:20]))
+println("Part 2: ", netVolume(input))
